48452a0e5df51e20453337e4af8cb72427ea8e87
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-26 11:13:14 -0500

    Updated my About Me section in my protfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,10 +18,10 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://drive.google.com/file/d/115P3Q4k_3RtqhvJuCjfq73fFP3qBhADl/view?usp=sharing', // if no resume, the button will not show up
+  paragraphOne: 'We are only here for so long, so it really makes sense to love what we do! The core reason I transitioned into tech is because of its everyday use and the feel that I am just scratching the surface! I am constantly being drawn in the deeper I dive and more I begin to understand about software engineering.',
+  paragraphTwo: 'Being able to create usable art out of thin air, continuously learning to improve my code, and understanding algorithms, data structures, and bugs within software is a dream come true!',
+  paragraphThree: ' ',
+  resume: 'https://drive.google.com/file/d/115P3Q4k_3RtqhvJuCjfq73fFP3qBhADl/view?usp=sharing'
 };
 
 // PROJECTS DATA
@@ -30,7 +30,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'Project1.png',
     title: 'KnowCal',
-    info: 'KnowCal is a user- friendly calorie tracking RESTful API, that generates recommended calorie consumption based on user input, stores daily food diary broken down into meals and snacks, in a database, shows data visualization and analysis of calorie consumption through charts, and gives the user a better experience with the implementation of third party APIs.',
+    info: '(Solo Project, 10 day sprint) KnowCal is a user- friendly calorie tracking RESTful API, that generates recommended calorie consumption based on user input, stores daily food diary broken down into meals and snacks, in a database, shows data visualization and analysis of calorie consumption through charts, and gives the user a better experience with the implementation of third party APIs.',
     info2: 'Technologies: C#, ASP.Net Core/MVC, Entity Framework, JavaScript, HMTL5/CSS, Bootstrap,  SQL, Chart.js, Postman, SendGrid API, Nutritionix API ',
     url: 'https://github.com/jPigg-tech/KnowCal.git',
     repo: 'https://github.com/jPigg-tech/KnowCal.git', // if no repo, the button will not show up
@@ -39,7 +39,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'Project2.png',
     title: 'Movie Library',
-    info: 'A Web Application list database of movies from our self built RESTful API. Giving the user the options to GET, POST, PUT, DELETE to the SQL database.',
+    info: '(Group Project, 4 day sprint) A Web Application list database of movies from our self built RESTful API. Giving the user the options to GET, POST, PUT, DELETE to the SQL database.',
     info2: 'Technologies: ASP.NET MVC, HTML, CSS, JavaScript, AJAX, jQuery, Postman',
     url: 'https://github.com/kyledobash/Movie_Library_Git_KD_KP_JP',
     repo: 'https://github.com/kyledobash/Movie_Library_Git_KD_KP_JP', // if no repo, the button will not show up
@@ -48,7 +48,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'Project3.png',
     title: 'Video Game Data',
-    info: '',
+    info: '(Group Project, 5 day sprint)',
     info2: 'Technologies: Python, Flask, HTML, CSS/Bootstrap, Postman, Chart.js',
     url: 'https://github.com/jPigg-tech/VideoGameDataProj',
     repo: 'https://github.com/jPigg-tech/VideoGameDataProj', // if no repo, the button will not show up
