6a910033208e2025bc00aabe062d1edc97c439ec
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-06-03 17:21:49 -0500

    Added a description in Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import PortfolioContext from '../../context/context';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
-  const { title, name, subtitle, cta } = hero;
+  const { title, name, subtitle, description, cta } = hero;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -27,9 +27,10 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Jeffery Pigg'}</span>
             <br />
-            {subtitle || "I'm a Software Engineer."}
+            {subtitle || "I'm a Software Developer."}
+            {description || "Check out some of the projects I've worked on recently!"}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
