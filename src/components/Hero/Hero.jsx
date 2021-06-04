d8ab2f88ee9b65f7f870dc44ac4ee020363d5af9
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-06-04 14:57:30 -0500

    Updated the description

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -30,7 +30,6 @@ const Header = () => {
             <span className="text-color-main">{name || 'Jeffery Pigg'}</span>
             <br />
             {subtitle || "I'm a Software Developer."}
-            {description || "Check out some of the projects I've worked on recently!"}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
